ad9eed6a4863dfe2df708fcb8e54e4d8fac14f4b
Jose <jhuertom@gmail.com>
2023-06-15 13:29:58 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'Paquete para mejorar los prints'
 LONG_DESCRIPTION = 'Paquete para mejorar los prints'
 
